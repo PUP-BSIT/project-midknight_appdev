e8b3dd7023683c50774f970a19f8ba5f53315ed1
Jeff Carl Monroy <jeffcmonroy@gmail.com>
2024-05-25 00:15:20 +0800

    add login to User.js

--- kajas/backend/models/User.js
+++ kajas/backend/models/User.js
@@ -1,17 +1,29 @@
-const db = require('../config/db');
+const db = require("../config/db");
 
 const createUser = (userInfoId, username, email, password, callback) => {
-  const query = 'INSERT INTO user (user_information_id, username, email, password) VALUES (?, ?, ?, ?)';
+  const query =
+    "INSERT INTO user (user_information_id, username, email, password) VALUES (?, ?, ?, ?)";
   db.query(query, [userInfoId, username, email, password], callback);
 };
 
 const verifyUser = (email, callback) => {
   const verify = 1;
-  const query = 'UPDATE user SET is_verify = ? WHERE email = ?';
+  const query = "UPDATE user SET is_verify = ? WHERE email = ?";
   db.query(query, [verify, email], callback);
-}
+};
+
+const findUserByEmail = (email, callback) => {
+  const query = "SELECT * FROM user WHERE email = ?";
+  db.query(query, [email], (err, results) => {
+    if (err) {
+      return callback(err, null);
+    }
+    return callback(null, results[0]);
+  });
+};
 
 module.exports = {
   createUser,
-  verifyUser
+  verifyUser,
+  findUserByEmail,
 };
