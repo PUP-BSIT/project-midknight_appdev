33eb828c33a8cd6e7816782a0fbd1ecbc879ebf3
SLValerio <vsirlance@gmail.com>
2024-05-28 20:46:19 +0800

    add dynamic form validation to forgot pass

--- kajas/src/app/forgot-password/forgot-password.component.ts
+++ kajas/src/app/forgot-password/forgot-password.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, Renderer2 } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ModalService } from '../../services/modal.service'; 
 import axios from 'axios';
@@ -24,27 +24,34 @@ export class ForgotPasswordComponent implements OnInit, OnDestroy {
     private modalService: ModalService 
   ) {
     this.forgotPasswordForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]]
+      email: ['', [Validators.required, Validators.email, this.gmailValidator]]
     });
   }
 
   ngOnInit(): void {
+    this.setInitialStyles();
+    window.addEventListener('resize', this.applyBackground.bind(this));
+  }
+
+  ngOnDestroy(): void {
+    this.revertStyles();
+    window.removeEventListener('resize', this.applyBackground.bind(this));
+  }
+
+  private setInitialStyles(): void {
     this.renderer.setStyle(document.body, 'height', '100%');
     this.renderer.setStyle(document.body, 'overflow', 'hidden');
     this.renderer.setStyle(document.body, 'display', 'flex');
     this.renderer.setStyle(document.body, 'justify-content', 'center');
     this.renderer.setStyle(document.body, 'align-items', 'center');
-    this.renderer.setStyle(document.body, 'background', 'url("../../assets/login_bg.png") center/cover no-repeat');
+    this.renderer.setStyle(document.body, 'background', 'url("../../assets/signup_bg.png") center/cover no-repeat');
     
     this.renderer.setStyle(document.documentElement, 'height', '100%');
     this.renderer.setStyle(document.documentElement, 'overflow', 'hidden');
     this.applyBackground();
-    window.addEventListener('resize', () => {
-      this.applyBackground();
-    });
   }
 
-  ngOnDestroy(): void {
+  private revertStyles(): void {
     this.renderer.removeStyle(document.body, 'height');
     this.renderer.removeStyle(document.body, 'overflow');
     this.renderer.removeStyle(document.body, 'display');
@@ -54,19 +61,23 @@ export class ForgotPasswordComponent implements OnInit, OnDestroy {
     
     this.renderer.removeStyle(document.documentElement, 'height');
     this.renderer.removeStyle(document.documentElement, 'overflow');
-    window.removeEventListener('resize', () => {});
   }
 
-  applyBackground(): void {
+  private applyBackground(): void {
     if (window.innerWidth <= 425) {
-      this.renderer.setStyle(document.body, 'background', 'url("../../assets/login_mbg.png") center/cover no-repeat');
+      this.renderer.setStyle(document.body, 'background', 'url("../../assets/signup_mbg.png") center/cover no-repeat');
     } else {
-      this.renderer.setStyle(document.body, 'background', 'url("../../assets/login_bg.png") center/cover no-repeat');
+      this.renderer.setStyle(document.body, 'background', 'url("../../assets/signup_bg.png") center/cover no-repeat');
     }
   }
 
-  get f() { return this.forgotPasswordForm.controls; }
-
+  private gmailValidator(control: AbstractControl): ValidationErrors | null {
+    if (control.value && !control.value.endsWith('@gmail.com')) {
+      return { notGmail: true };
+    }
+    return null;
+  }
+  
   async onSubmit(): Promise<void> {
     const url = "http://localhost:4000";
     this.submitted = true;
@@ -85,11 +96,25 @@ export class ForgotPasswordComponent implements OnInit, OnDestroy {
         this.showModal = true;
       }
     } catch (error) {
-      this.modalMessage = 'Email does not exist. Please try again!';
+      this.modalMessage = 'That email address is not linked to a Kajas account. Try again or create a new one.';
       this.showModal = true;
       console.error('Error sending the email for resetting the password:', error);
       this.errorMessage = error.response?.data?.message || 'Something went wrong. Please try again later.';
     }
+  }
+
+  getEmailErrorMessage() {
+    const emailControl = this.forgotPasswordForm.get('email');
+    if (emailControl.hasError('required')) {
+      return 'Email is required.';
+    } else if (emailControl.hasError('email')) {
+      return 'Must be a valid email address.';
+    }  else if (emailControl.errors.notGmail) {
+      return 'Email must be a valid Google mail address.';
+    } else if (emailControl.hasError('serverError')) {
+      return emailControl.getError('serverError');
+    }
+    return '';
   }
 
   onCancel() {
