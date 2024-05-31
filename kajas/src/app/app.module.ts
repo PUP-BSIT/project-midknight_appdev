099130b5d434ac7b21afb64f6c2032507480f189
Jeff Carl Monroy <jeffcmonroy@gmail.com>
2024-06-01 06:25:39 +0800

    modify routing for profile module

--- kajas/src/app/app.module.ts
+++ kajas/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ModalMessageComponent } from './shared/modal-message/modal-message.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { HeaderComponent } from './header/header.component';
     ForgotPasswordComponent,
     ModalMessageComponent,
     ResetPasswordComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
