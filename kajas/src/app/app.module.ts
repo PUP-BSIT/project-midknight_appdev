c69adb3e7b98eacca974926eca0200dcdb239044
KRAyacocho <kylieross.ayacocho@gmail.com>
2024-06-11 20:38:49 +0800

    add artwork details files

--- kajas/src/app/app.module.ts
+++ kajas/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { SetupProfileComponent } from './setup-profile/setup-profile.component';
 import { LocationService } from '../services/location.service';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { AddArtworkComponent } from './add-artwork/add-artwork.component';
+import { ArtworkDetailsComponent } from './artwork-details/artwork-details.component';
 
 
 @NgModule({
@@ -37,7 +38,8 @@ import { AddArtworkComponent } from './add-artwork/add-artwork.component';
     ProfileComponent,
     SetupProfileComponent,
     EditProfileComponent,
-    AddArtworkComponent
+    AddArtworkComponent,
+    ArtworkDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ kajas/src/app/artwork-details/artwork-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artwork-details',
+  templateUrl: './artwork-details.component.html',
+  styleUrl: './artwork-details.component.css'
+})
+export class ArtworkDetailsComponent {
+
+}
