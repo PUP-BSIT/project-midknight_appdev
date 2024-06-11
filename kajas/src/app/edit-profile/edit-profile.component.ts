757173c1281d4c62617560aad84a6f4499c5d6b6
SLValerio <vsirlance@gmail.com>
2024-06-11 12:16:16 +0800

    fix edit profile picture bug

--- kajas/src/app/edit-profile/edit-profile.component.ts
+++ kajas/src/app/edit-profile/edit-profile.component.ts
@@ -41,7 +41,7 @@ export class EditProfileComponent implements OnInit {
   ) {
     this.profileForm = this.fb.group({
       id: [this.sessionStorage.get('id')],
-      profile: [''],
+      profile: [this.sessionStorage.get('profile')],
       bio: [this.sessionStorage.get('bio'), {
         validators: [
           Validators.maxLength(250)
@@ -198,7 +198,7 @@ export class EditProfileComponent implements OnInit {
         this.sessionStorage.set('city', this.profileForm.controls.city.value);
         this.sessionStorage.set('country', this.profileForm.controls.country.value);
         this.sessionStorage.set('bio', this.profileForm.controls.bio.value);
-        this.sessionStorage.set('profile', response.data.updatedprofile);
+        this.sessionStorage.set('profile', response.data.updatedprofile || this.sessionStorage.get('profile'));
         this.sessionStorage.set('linkedin', this.profileForm.controls.linkedIn.value);
         this.sessionStorage.set('facebook', this.profileForm.controls.facebook.value);
         this.sessionStorage.set('instagram', this.profileForm.controls.instagram.value);
