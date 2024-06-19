413bbb4b216f026a97af5e7187a862ea8ca8ac85
SLValerio <vsirlance@gmail.com>
2024-06-19 16:17:57 +0800

    minor settings ui adjustments

--- kajas/src/app/settings/settings.component.ts
+++ kajas/src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -8,7 +9,15 @@ import { Component } from '@angular/core';
 export class SettingsComponent {
   selectedTab: string = 'change-email';
 
+  constructor(
+    private router: Router
+  ) {}
+
   selectTab(tab: string) {
     this.selectedTab = tab;
+  }
+
+  close():void{
+    this.router.navigateByUrl('/profile');
   }
 }
